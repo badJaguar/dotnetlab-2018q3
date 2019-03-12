325f64ff3ca4c3a2d41a5e1fadf2e2a523837267
Aleksandr Ataakgayev <bad.bad.jaguar@gmail.com>
2019-03-12 17:14:32 +0300

    Added Order entity and Customer withount implementation.

---
+++ ADO.NET_ORM/Example/DAL/Entities/Order.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace DAL.Entities
+{
+    /// <summary>
+    /// Represents an order.
+    /// </summary>
+    public class Order
+    {
+        /// <summary>
+        /// Gets or sets an order ID.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets a customer ID.
+        /// </summary>
+        public int CustomerId { get; set; }
+        
+        /// <summary>
+        /// Gets or sets an ordered customer.
+        /// </summary>
+        public virtual Customer Customer { get; set; }
+
+        /// <summary>
+        /// Gets or sets a collection of ordered items.
+        /// </summary>
+        public virtual ICollection<Item> ItemCollection { get; set; }
+    }
+}
