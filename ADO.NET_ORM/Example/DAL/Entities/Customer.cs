202bab5841e048376367105810702984c5a6d396
Aleksandr Ataakgayev <bad.bad.jaguar@gmail.com>
2019-03-13 21:16:36 +0300

    Add Customer entity model. Add CustomerConfiguration class

---
+++ ADO.NET_ORM/Example/DAL/Entities/Customer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace DAL.Entities
+{
+    /// <summary>
+    /// Represents a customer entity.
+    /// </summary>
+    public class Customer
+    {
+        /// <summary>
+        /// Gets or sets customers ID.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets customers company name.
+        /// </summary>
+        public string CompanyName { get; set; }
+
+        /// <summary>
+        /// Gets or sets customers address (street, building)
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// Gets or sets customer location  city.
+        /// </summary>
+        public string City { get; set; }
+
+        /// <summary>
+        /// Gets or sets customers location state.
+        /// </summary>
+        public string State { get; set; }
+
+        /// <summary>
+        /// Gets or sets customers collection orders.
+        /// </summary>
+        public ICollection<Order> Orders { get; set; }
+    }
+}
--- ADO.NET_ORM/Example/DAL/Migrations/201903131417178_OrderItemUpdated.cs
+++ ADO.NET_ORM/Example/DAL/Migrations/201903131417178_OrderItemUpdated.cs
@@ -8,7 +8,12 @@ namespace DAL.Migrations
         public override void Up()
         {
             DropPrimaryKey("dbo.tbl_order_items");
-            AddPrimaryKey("dbo.tbl_order_items", new[] { "cln_order_id", "cln_item_id", "cln_qty" });
+            AddPrimaryKey("dbo.tbl_order_items", new[]
+            {
+                "cln_order_id",
+                "cln_item_id",
+                "cln_qty"
+            });
 
             Sql("update tbl_order_items set cln_qty = 4 where cln_order_id = 1 and cln_item_id = 1"); 
             Sql("update tbl_order_items set cln_qty = 5 where cln_order_id = 1 and cln_item_id = 2"); 
@@ -21,7 +26,12 @@ namespace DAL.Migrations
         public override void Down()
         {
             DropPrimaryKey("dbo.tbl_order_items");
-            AddPrimaryKey("dbo.tbl_order_items", new[] { "cln_item_id", "cln_order_id", "cln_qty" });
+            AddPrimaryKey("dbo.tbl_order_items", new[]
+            {
+                "cln_item_id",
+                "cln_order_id",
+                "cln_qty"
+            });
         }
     }
 }
