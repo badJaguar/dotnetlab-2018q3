143fd58abadbd892c9dde25494d7db11477fca99
Aleksandr Ataakgayev <bad.bad.jaguar@gmail.com>
2019-03-13 20:20:06 +0300

    End TaskPointThree

--- ADO.NET_ORM/Example/DAL/Migrations/201903131417178_OrderItemUpdated.cs
+++ ADO.NET_ORM/Example/DAL/Migrations/201903131417178_OrderItemUpdated.cs
@@ -10,12 +10,12 @@ namespace DAL.Migrations
             DropPrimaryKey("dbo.tbl_order_items");
             AddPrimaryKey("dbo.tbl_order_items", new[] { "cln_order_id", "cln_item_id", "cln_qty" });
 
-            Sql("update tbl_order_items set cln_qty = 4 where cln_order_id = 1 and cln_item_id = 1,"+
-               "update tbl_order_items set cln_qty = 5 where cln_order_id = 1 and cln_item_id = 2,"+
-               "update tbl_order_items set cln_qty = 32 where cln_order_id = 1 and cln_item_id = 3");
+            Sql("update tbl_order_items set cln_qty = 4 where cln_order_id = 1 and cln_item_id = 1"); 
+            Sql("update tbl_order_items set cln_qty = 5 where cln_order_id = 1 and cln_item_id = 2"); 
+            Sql("update tbl_order_items set cln_qty = 32 where cln_order_id = 1 and cln_item_id = 3");
 
-            Sql("update tbl_order_items set cln_qty = 500 where cln_order_id = 2 and cln_item_id = 1,"+
-                "update tbl_order_items set cln_qty = 750 where cln_order_id = 2 and cln_item_id = 2");
+            Sql("update tbl_order_items set cln_qty = 500 where cln_order_id = 2 and cln_item_id = 1");
+            Sql("update tbl_order_items set cln_qty = 750 where cln_order_id = 2 and cln_item_id = 2");
         }
         
         public override void Down()
