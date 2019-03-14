11e9ddfaf7e297cb22688b8f12994556a5ccf5a2
Aleksandr Ataakgayev <bad.bad.jaguar@gmail.com>
2019-03-14 18:50:39 +0300

    Add InitialCreate migration. Add configuration ItemConfiguration. Seeded database

---
+++ ADO.NET_ORM/Example/DAL/Configuration/ItemConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using DAL.Entities;
+
+namespace DAL.Configuration
+{
+    public class ItemConfiguration : EntityTypeConfiguration<Item>
+    {
+        public ItemConfiguration()
+        {
+            ToTable("tbl_items").HasKey(item => item.Id);
+            Property(item => item.Id).HasColumnName("cln_id");
+            Property(item => item.Description).HasColumnName("cln_description");
+            Property(item => item.Price).HasColumnName("cln_price");
+        }
+    }
+}
--- ADO.NET_ORM/Example/DAL/Context/AppDbContext.cs
+++ ADO.NET_ORM/Example/DAL/Context/AppDbContext.cs
@@ -6,17 +6,16 @@ namespace DAL.Context
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext() : base("InternationWidgets")
+        public AppDbContext() : base("InternationalWidgets")
         {
-            
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<AppDbContext, Migrations.Configuration>());
         }
 
         public DbSet<Item> Items { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            //TODO: setup dbcontext configuration using EF Fluent API. DO NOT add property attributes for any files from "Entities" folder.
-
+            modelBuilder.Configurations.Add(new ItemConfiguration());
             base.OnModelCreating(modelBuilder);
         }
     }
